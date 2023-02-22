32b75638fe9ab31df661153cf5f5bbc2da1916de
Abas Kamara <kamaraabas@aol.com>
2023-02-22 17:37:22 -0500

    dark mode working

---
+++ frontend/hooks/useLocalStorage.js
@@ -0,0 +1,20 @@
+import { useState } from 'react';
+
+const useLocalStorge = (key, initialValue) => {
+    const [state, setState] = useState(() => {
+        if (localStorage.getItem(key)){
+            return (JSON.parse(localStorage.getItem(key)));
+        }
+            localStorage.setItem(key, JSON.stringify(initialValue));
+            return initialValue;
+    });
+
+    const setStoredState = (value) => {
+        localStorage.setItem(key, JSON.stringify(value));
+        setState(value);
+    };
+    return([state, setStoredState]);
+
+};
+
+export default useLocalStorge;
